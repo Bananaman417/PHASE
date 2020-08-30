811f3ea4d881ecadeb9d7ba5a5dde5126775a02c
BooNoona <67639354+Bananaman417@users.noreply.github.com>
2020-08-30 06:48:32 -0700

    Initial commit

---
+++ Assets/Controller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float speed;
+    public float jumpforce;
+    private float moveinput;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        moveinput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveinput * speed, rb.velocity.y);
+    }
+
+
+
+  
+void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpforce;
+        }
+
+    }
+
+
+}
+
