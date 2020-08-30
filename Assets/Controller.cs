708786094a8d7aa6bcd0f6b56d15c4bc5bb71ca1
CPU <claudio.pucci@gmail.com>
2020-08-30 16:51:08 +0200

    Fixed the jump and added a few layers

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -2,38 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controller : MonoBehaviour
-{
-    public float speed;
-    public float jumpforce;
-    private float moveinput;
-    private Rigidbody2D rb;
-    private bool isGrounded;
+public class Controller : MonoBehaviour {
+  public float speed;
+  public float jumpforce;
+  private float moveinput;
+  private Rigidbody2D rb;
+  public bool isGrounded;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
+  public LayerMask groundLayer;
+
+  // Start is called before the first frame update
+  void Start() {
+    rb = GetComponent<Rigidbody2D>();
+  }
+
+  void FixedUpdate() {
+    moveinput = Input.GetAxis("Horizontal");
+    rb.velocity = new Vector2(moveinput * speed, rb.velocity.y);
+  }
+
+
+
+
+  void Update() {
+    if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true) {
+      rb.velocity += Vector2.up * jumpforce;
     }
 
-    void FixedUpdate()
-    {
-        moveinput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(moveinput * speed, rb.velocity.y);
+  }
+
+  private void OnCollisionEnter2D(Collision2D collision) {
+    if ((groundLayer & (1 << collision.collider.gameObject.layer)) != 0) {
+      isGrounded = true;
     }
-
-
-
-  
-void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
-        {
-            rb.velocity = Vector2.up * jumpforce;
-        }
-
-    }
-
+  }
 
 }
 
